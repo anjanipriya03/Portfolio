a9d4002e24ae5b5c25ac1b890d8f98f1a8629589
anjanipriya03 <anjanipriyachalla@gmail.com>
2026-07-30 22:53:24 +0530

    good one original one

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import About from "./routes/About";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
 
-
-import {Route,Routes} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-   <>
-   <Routes>
-    <Route path="/" element={<Home/>} />
-    <Route path="/project" element={<Project/>} />
-    <Route path="/about" element={<About/>} />
-    <Route path="/contact" element={<Contact/>} />
-    
-    
-   </Routes>
-   </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
